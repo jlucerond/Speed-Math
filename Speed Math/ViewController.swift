d30514e9ec9f2fd7f9b0c1b348a18d9e1388de59
Joe Lucero <jlucerond@gmail.com>
2016-09-15 16:18:50 -0400

    basic outline game is complete

--- Speed Math/ViewController.swift
+++ Speed Math/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var directionsLabel: UILabel!
+    
+    var maxNumberComputation = 10
+    var computationName = "adding"
+    var computationCodeInt = 0
+
+    // View Controller Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDirectionsLabel()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // IBActions
+    @IBAction func sliderMoved(sender: UISlider) {
+        maxNumberComputation = Int(sender.value)
+        updateDirectionsLabel()
+    }
+    @IBAction func computationButtonSelected(sender: UISegmentedControl) {
+        computationCodeInt = sender.selectedSegmentIndex
+        
+        
+        if computationCodeInt == 0 {
+            computationName = "adding"
+        }
+        else if computationCodeInt == 1 {
+            computationName = "subtracting"
+        }
+        else if computationCodeInt == 2 {
+            computationName = "multiplying"
+        }
+        else if computationCodeInt == 3 {
+            computationName = "dividing"
+        }
+        
+        updateDirectionsLabel()
+    }
+    @IBAction func startNewGameButtonPressed(sender: UIButton) {
     }
 
+    // Custom Functions
+    func updateDirectionsLabel () {
+
+        
+        directionsLabel.text = "You'll be \(computationName) numbers between 1-\(maxNumberComputation)"
+    }
 
 }
 

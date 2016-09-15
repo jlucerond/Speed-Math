882aabdf63549fb664254d2c82e1ea2ceee51519
Joe Lucero <jlucerond@gmail.com>
2016-09-15 15:06:25 -0400

    Initial Commit

---
+++ Speed Math/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speed Math
+//
+//  Created by Joe Lucero on 9/15/16.
+//  Copyright © 2016 Joe Lucero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
